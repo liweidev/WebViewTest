cfec2bc4d16084f4e90a894091bfcb41c6366aaa
liwei <675424756@qq.com>
2017-03-02 17:17:48 +0800

    添加到远程代码仓库

---
+++ app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.webviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+	private int i=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView= (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.baidu.com");
+    }
+}
