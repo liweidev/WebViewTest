91b7ea6ee66e5c5f45aae3d417799855282fc49e
liwei <675424756@qq.com>
2017-03-02 17:45:08 +0800

    增加了int i=j

--- app/src/main/java/com/example/webviewtest/MainActivity.java
+++ app/src/main/java/com/example/webviewtest/MainActivity.java
@@ -8,6 +8,7 @@ import android.webkit.WebViewClient;
 public class MainActivity extends AppCompatActivity {
 
 	private int i=0;
+    private int j=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
